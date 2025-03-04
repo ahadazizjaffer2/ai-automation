c74fbd959ad78b926ee55522db08ccf9269b10ee
Ahad Aziz Jaffer <k213241@nu.edu.pk>
2025-03-05 02:00:37 +0500

    try2

--- src/routes/Campaigns.jsx
+++ src/routes/Campaigns.jsx
@@ -111,7 +111,7 @@ function App() {
     }
   };
   return (
-    <div className="min-h-screen bg-gray-50">
+    <div className="min-h-screen bg-gray-50 overflow-hidden">
       {/* Main content */}
       <main className="max-w-9xl mx-auto px-4 sm:px-6 lg:px-8 py-6">
         {/* Filters and actions */}
